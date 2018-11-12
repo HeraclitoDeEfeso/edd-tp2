76f1688f0111ae57a4eddf7c37ef46d12fc6aaed
Matías Monti <matiasmonti@hotmail.com>
2018-11-12 15:37:39 -0300

    el arbol no admite repetidos, imprime bien, y hay una logica con el buscar e insertar que permitiria agregar un documento en la lista de aparicion correspondiente si tan solo existiria ja.

--- Btree.py
+++ Btree.py
@@ -28,7 +28,7 @@ class BTree(object):
 
             # Agrega al padre el nuevo nodo con las keys de mayor valor y devuelve el nodo que contendra la nueva key
             padre.hijo = padre.insertar_hijo(nuevo_nodo)
-            if nueva_key < key_medio:
+            if nueva_key[0] < key_medio[0]:
                 return self
             else:
                 return nuevo_nodo
@@ -47,8 +47,8 @@ class BTree(object):
 
         def insertar_hijo(self, nuevo_nodo):
             """agrega un hijo al nodo y ordena a todos los hijos.Devuelve una lista ordenada de los nodos hijos"""
-            i = len(self.hijo) - 1
-            while i >= 0 and self.hijo[i].keys[0] > nuevo_nodo.keys[0]:
+            i = len(self.hijo) - 1 #ultimo hijo
+            while i >= 0 and self.hijo[i].keys > nuevo_nodo.keys:
                 i -= 1
             return self.hijo[:i + 1] + [nuevo_nodo] + self.hijo[i + 1:]
 
@@ -59,40 +59,45 @@ class BTree(object):
             raise ValueError("El orden del arbol debe ser 2 o superior")
         self.raiz = self.Nodo(t)
 
-    def insertar(self, nueva_key):
+    def insertar(self, nueva_key, documento=None):
         """Inserta un key con un valor determinado"""
-        nodo = self.raiz
-        # El split de la raiz es manejada de forma particular(nuestro metodo split necesita un padre)
-        if nodo._is_full:
-            new_raiz = self.Nodo(self._t)
-            new_raiz.hijo.append(self.raiz)
-            new_raiz.hoja = False
-            # nodo sera el nodo que contendra la nueva key agregada y redefine la raiz
-            nodo = nodo.split(new_raiz, nueva_key)
-            self.raiz = new_raiz
+        if not self.buscar(nueva_key, documento):
+            
+            nodo = self.raiz
+            # El split de la raiz es manejada de forma particular(nuestro metodo split necesita un padre)
+            if nodo._is_full:
+                new_raiz = self.Nodo(self._t)
+                new_raiz.hijo.append(self.raiz)
+                new_raiz.hoja = False
+                # nodo sera el nodo que contendra la nueva key agregada y redefine la raiz
+                nodo = nodo.split(new_raiz, nueva_key)
+                self.raiz = new_raiz
 
-        while not nodo.hoja:
-            i = nodo.size - 1  # ultima key
-            while i > 0 and nueva_key < nodo.keys[i]:
-                i -= 1  # posicion de la nueva key o la mas grande de las que son menores a la nueva key
-            if nueva_key > nodo.keys[i]:
-                i += 1
-                # si keys[i] es la mas grande de las menores a nueva_key, la convierto en la menor de las mayores
+            while not nodo.hoja:
+                i = nodo.size - 1  # ultima key
+                while i > 0 and nueva_key < nodo.keys[i]:
+                    i -= 1  # posicion de la nueva key o la mas grande de las que son menores a la nueva key
+                if nueva_key > nodo.keys[i]:
+                    i += 1
+                    # si keys[i] es la mas grande de las menores a nueva_key, la convierto en la menor de las mayores
 
-            # proximo sera el nodo que sigue en el camino hasta ser la hoja que contendra la nueva key
-            proximo = nodo.hijo[i]
-            if proximo._is_full:
-                nodo = proximo.split(nodo, nueva_key)
-            else:
-                nodo = proximo
-        # Como spliteamos todos los nodos completos en el camino a la hoja indicada podemos simplemente agregar la key.
-        nodo.agregar_key(nueva_key)
+                # proximo sera el nodo que sigue en el camino hasta ser la hoja que contendra la nueva key
+                proximo = nodo.hijo[i]
+                if proximo._is_full:
+                    nodo = proximo.split(nodo, nueva_key)
+                else:
+                    nodo = proximo
+            # Como spliteamos todos los nodos completos en el camino a la hoja indicada podemos simplemente agregar la key.
+            nodo.agregar_key(nueva_key)
 
-    def buscar(self, valor, nodo=None):
+    def buscar(self, valor, nodo=None, documento=None):
         """Devuelve True el valor esta en el arbol"""
         if nodo is None:
             nodo = self.raiz
         if valor in nodo.keys:
+            if documento != None:
+                #agrego el documento a su lista de aparicion
+                """la lista de aparicion no esta implementada aun"""
             return True
         elif nodo.hoja:  # no hay mas hijos que chequear.
             return False
@@ -113,5 +118,5 @@ class BTree(object):
                 if nodo.hijo:
                     prox_nivel.extend(nodo.hijo)
                 output += str(nodo.keys) + " "
-            print(output)
-            este_nivel = prox_nivel
+                print(output)
+                este_nivel = prox_nivel
