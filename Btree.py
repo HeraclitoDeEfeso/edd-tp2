6bcf0348e2efb7579e22fc6fa6589338e688aae2
MatiasMonti <33459048+MatiasMonti@users.noreply.github.com>
2018-11-15 23:08:42 -0300

    arbol b con listas de aparicion
    Le faltan retoques como por ejemplo que hacer en el caso que los datos de entrada no sean los adecuados.
    Pero anda bien utilizado

--- Btree.py
+++ Btree.py
@@ -3,28 +3,25 @@ class BTree(object):
 
     class Nodo(object):
 
-        def __init__(self, t):
+        def __init__(self, t, hoja=True):
             self.keys = []  # elementos del nodo
             self.hijo = []  # hijos(otros nodos internos o el contenido de la hoja en caso de serlo)
-            self.hoja = True  # me permite saber si sus hijos son otros nodos o datos
+            self.hoja = hoja  # me permite saber si sus hijos son otros nodos o datos
             self._t = t  # t = orden
 
         def split(self, padre, nueva_key):
             """divide el nodo y reasigna keys e hijos,devuelve el nodo que contendra a la nueva key"""
-            nuevo_nodo = self.__class__(self._t)
+            nuevo_nodo = self.__class__(self._t, self.hoja)
             mitad = self.size // 2
             key_medio = self.keys[mitad]
-            padre.agregar_key(key_medio)
+            padre.keys.append(key_medio)
+            padre.keys.sort()
 
             # Agrega las keys y los hijos al nodo correcto,el nuevo nodo siempre contendra las keys mas altas
             nuevo_nodo.hijo = self.hijo[mitad :]
             self.hijo = self.hijo[:mitad ]
             nuevo_nodo.keys = self.keys[mitad :]
             self.keys = self.keys[:mitad]
-
-            # Si el nuevo_nodo tiene hijo, es un nodo interno
-            if len(nuevo_nodo.hijo) > 0:
-                nuevo_nodo.hoja = False
 
             # Agrega al padre el nuevo nodo con las keys de mayor valor y devuelve el nodo que contendra la nueva key
             padre.hijo = padre.insertar_hijo(nuevo_nodo)
@@ -41,9 +38,19 @@ class BTree(object):
         def size(self):
             return len(self.keys)
 
-        def agregar_key(self, valor):
+        def agregar_key(self, valor,documento):
             self.keys.append(valor)
-            self.keys.sort()
+            self.hijo.append([documento])
+            i = len(self.keys)-1
+            while i >= 1 and self.keys[i] < self.keys[i-1]:
+                self.swap(self.keys,i)
+                self.swap(self.hijo,i)
+                i-=1
+
+        def swap(self, lista, indice):
+            aux = lista[indice]
+            lista[indice] = lista[indice-1]
+            lista[indice-1] = aux
 
         def insertar_hijo(self, nuevo_nodo):
             """agrega un hijo al nodo y ordena a todos los hijos.Devuelve una lista ordenada de los nodos hijos"""
@@ -59,53 +66,47 @@ class BTree(object):
             raise ValueError("El orden del arbol debe ser 2 o superior")
         self.raiz = self.Nodo(t)
 
-    def insertar(self, nueva_key, documento=None):
+    def insertar(self, nueva_key, documento):
         """Inserta un key con un valor determinado"""
-        if not self.buscar(nueva_key, documento):
-            
-            nodo = self.raiz
-            # El split de la raiz es manejada de forma particular(nuestro metodo split necesita un padre)
-            if nodo._is_full:
-                new_raiz = self.Nodo(self._t)
-                new_raiz.hijo.append(self.raiz)
-                new_raiz.hoja = False
-                # nodo sera el nodo que contendra la nueva key agregada y redefine la raiz
-                nodo = nodo.split(new_raiz, nueva_key)
-                self.raiz = new_raiz
+        nodo = self.raiz
+        # El split de la raiz es manejada de forma particular(nuestro metodo split necesita un padre)
+        if nodo._is_full:
+            new_raiz = self.Nodo(self._t, False)
+            new_raiz.hijo.append(self.raiz)
+            new_raiz.hoja = False
+            # nodo sera el nodo que contendra la nueva key agregada y redefine la raiz
+            nodo = nodo.split(new_raiz, nueva_key)
+            self.raiz = new_raiz
+        while not nodo.hoja:
+            i = nodo.size - 1  # ultima key
+            while i > 0 and nueva_key < nodo.keys[i]:
+                i -= 1  # posicion de la nueva key o la mas grande de las que son menores a la nueva key
+            if nueva_key > nodo.keys[i]:
+                i += 1  # ahora i sera la posicion mayor mas proxima a la nueva key
+            # proximo sera el nodo que sigue en el camino hasta ser la hoja que contendra la nueva key
+            proximo = nodo.hijo[i]
+            if proximo._is_full:
+                nodo = proximo.split(nodo, nueva_key)
+            else:
+                nodo = proximo
+        if not self.buscar(nueva_key):
+            # Como spliteamos todos los nodos completos podemos simplemente agregar la key.
+            nodo.agregar_key(nueva_key,documento)#aca agrega la key y el documento a la lista de aparicion.
+        else:
+            nodo.hijo[nodo.keys.index(nueva_key)].append(documento)
 
-            while not nodo.hoja:
-                i = nodo.size - 1  # ultima key
-                while i > 0 and nueva_key < nodo.keys[i]:
-                    i -= 1  # posicion de la nueva key o la mas grande de las que son menores a la nueva key
-                if nueva_key > nodo.keys[i]:
-                    i += 1
-                    # si keys[i] es la mas grande de las menores a nueva_key, la convierto en la menor de las mayores
-
-                # proximo sera el nodo que sigue en el camino hasta ser la hoja que contendra la nueva key
-                proximo = nodo.hijo[i]
-                if proximo._is_full:
-                    nodo = proximo.split(nodo, nueva_key)
-                else:
-                    nodo = proximo
-            # Como spliteamos todos los nodos completos en el camino a la hoja indicada podemos simplemente agregar la key.
-            nodo.agregar_key(nueva_key)#aca ademas de agregar la key tambien agregaria el documento a la lista de aparicion.
-
-    def buscar(self, valor, nodo=None, documento=None):
+    def buscar(self, valor, nodo=None):
         """Devuelve True el valor esta en el arbol"""
         if nodo is None:
             nodo = self.raiz
         if valor in nodo.keys:
-            if documento != None:
-                #agrego el documento a su lista de aparicion
-                """la lista de aparicion no esta implementada aun"""
             return True
         elif nodo.hoja:  # no hay mas hijos que chequear.
             return False
         else:
             i = 0
             while i < nodo.size and valor > nodo.keys[i]:
-                i += 1
-                # hasta que i la pos del nodo hijo que podria contener el valor
+                i += 1  # hasta que i la pos del nodo hijo que podria contener el valor
             return self.buscar(valor, nodo.hijo[i])
 
     def imprimir_arbol(self):
@@ -115,7 +116,7 @@ class BTree(object):
             prox_nivel = []
             output = ""
             for nodo in este_nivel:
-                if nodo.hijo:
+                if not nodo.hoja:
                     prox_nivel.extend(nodo.hijo)
                 output += str(nodo.keys) + " "
             print(output)
@@ -124,23 +125,29 @@ class BTree(object):
 
 
 def prueba():
-    """ expected = [5, 12] 
-[1, 2, 3, 4] [5, 6, 7, 8, 9] [12, 13, 123] """
+    """ expected = [4, 6, 12]
+    [1, 2, 3] [4, 5] [6, 7, 8, 9] [12, 13, 123]
+    el numero dos aparece en los documentos :A, b, c, d"""
     b = BTree(3)
-    b.insertar(2)
-    b.insertar(12)
-    b.insertar(123)
-    b.insertar(13)
-    b.insertar(1)
-    b.insertar(5)
-    b.insertar(2)
-    b.insertar(9)
-    b.insertar(6)
-    b.insertar(1)
-    b.insertar(7)
-    b.insertar(8)
-    b.insertar(4)
-    b.insertar(3)
-    b.insertar(12)
-    b.insertar(2)
+    b.insertar(2,"A")
+    b.insertar(2,"b")
+    b.insertar(2,"c")
+    b.insertar(2,"d")
+    b.insertar(12, 1)
+    b.insertar(4, 1)
+    b.insertar(3, 1)
+    b.insertar(123,1)
+    b.insertar(13,1)
+    b.insertar(1,1)
+    b.insertar(5,1)
+    b.insertar(6,1)
+    b.insertar(9,1)
+    b.insertar(7,1)
+    b.insertar(8,1)
     b.imprimir_arbol()
+
+    print("el numero 2 aparece en los documentos :" + ", ".join(b.raiz.hijo[0].hijo[1]))
+
+if __name__ == "__main__":
+    prueba()
+
